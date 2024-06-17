a43a33e0b1136ce6dfea47ef78ab2167263c2c63
jinse11111 <3458422924@qq.com>
2024-06-17 16:39:20 +0800

    Update tz.js

--- tz.js
+++ tz.js
@@ -9,7 +9,7 @@ const customParam = urlParams.get('url');
 const randomString = Math.random().toString(36).substring(2); // 生成随机字符串，包括数字和小写字母
 
 // 构建跳转链接
-let redirectURL = "https://xcz789.github.io/x.html";
+let redirectURL = "http://static.yage.org.cn//assets/images/214/logo_yage.png";
 if (customParam) {
     redirectURL += "?url=" + customParam + "#" + randomString; // 将随机字符串添加到参数后面
 }
