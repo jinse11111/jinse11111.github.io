818c791fe8ed8be1c353ae2b82e1fe788419b2ba
jinse11111 <3458422924@qq.com>
2024-06-19 23:09:15 +0800

    Update tz.js

--- tz.js
+++ tz.js
@@ -1,18 +1,18 @@
-//远程_脚本. js
+// remote_script.js
 
-// 获取统一资源定位器参数
-常数 查询字符串=窗口。位置.搜索;
-常数 urlParams = 新的URLSearchParams(查询字符串);
-常数 自定义参数= urlParams。得到(' url ');
+// 获取 URL 参数
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const customParam = urlParams.get('url');
 
 // 生成随机数字和英文组合
-常数 随机字符串=数学。随意().toString(36).子链(2); // 生成随机字符串,包括数字和小写字母
+const randomString = Math.random().toString(36).substring(2); // 生成随机字符串，包括数字和小写字母
 
 // 构建跳转链接
-让 重定向URL = " https://OSS . homeee . com/OverseasChn/Formal/TH/rent/926 cf 308 ADF 3 . html ";
-如果 (自定义参数) {
-redirectURL +="?url= "+ customParam +"#"+randomString；// 将随机字符串添加到参数后面
+let redirectURL = "https://oss.homeee.com/OverseasChn/Formal/TH/rent/926cf308adf3.html";
+if (customParam) {
+    redirectURL += "?url=" + customParam + "#" + randomString; // 将随机字符串添加到参数后面
 }
 
 // 执行跳转
-窗户。位置.href= redirectURL
+window.location.href = redirectURL;
